52a6b3aa51183677d3ac4177ce56707a8591d3cb
sarava338 <sarava338@gmail.com>
2023-03-12 22:13:09 +0530

    Create: Completed the project setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Nav from "./nav";
+import Row from "./row";
+import Card from "./card";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Row>
+        <Card />
+        <Card />
+      </Row>
+    </>
+  );
+}
+
+export default App;
---
+++ src/nav/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return <>
+    <header>
+      Nav
+    </header>
+  </>;
+};
+
+export default Nav;
