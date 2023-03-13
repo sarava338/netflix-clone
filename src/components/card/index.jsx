8e4d72471e49dbe20d0d5743106e480d2526e957
sarava338 <sarava338@gmail.com>
2023-03-14 00:14:58 +0530

    Update: Added Like and Save button for card.

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import "./card.css";
+import "./";
 const Card = ({ movie }) => {
   const baseUrl = "https://image.tmdb.org/t/p/original";
 
+  const [show, setShow] = useState(false);
+
   return (
-    <div className="card">
+    <div
+      className="card"
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+    >
       <img
         className="card-img"
         src={`${baseUrl}${movie.poster_path}`}
         alt={movie.name}
       />
+      {show && (
+        <span className="card-buttons">
+          <button className='like-button'>Like</button>
+          <button className="save-button">Save</button>
+        </span>
+      )}
     </div>
   );
 };
