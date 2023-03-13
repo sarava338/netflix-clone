96fe2713ea06b57d5aec243f978b737af692258a
sarava338 <sarava338@gmail.com>
2023-03-13 22:58:57 +0530

    Create: Created Nav component with styles

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import "./nav.css";
 
 const Nav = () => {
-  return <>
-    <nav>
-      Nav
-    </nav>
-  </>;
+  const netflixLogo =
+    "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg";
+  return (
+    <>
+      <nav>
+        <img
+          src={netflixLogo}
+          alt="Netflix 2015 logo.svg"
+          width="120"
+          height="25"
+        />
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/sarava338/"
+        >
+          @ sarava338
+        </a>
+      </nav>
+    </>
+  );
 };
 
 export default Nav;
