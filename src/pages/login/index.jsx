949e3fcc98b1ea4cc88f674cf26f9148a261ce0e
sarava338 <sarava338@gmail.com>
2023-03-19 01:58:25 +0530

    Modify: Modified Login component

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -4,19 +4,27 @@ const Login = () => {
 
   const handleSubmit = () => {
     const form = document.getElementById('form')
-    alert(form.username.value)
+    const userCredential = {
+      username: form.username.value,
+      password: form.password.value
+    }
+    form.username.value = ''
+    form.password.value = ''
+    console.log(userCredential)
   }
 
   return (
     <>
         <form id='form' onSubmit={(e)=> e.preventDefault()}>
-            <label htmlFor="username"> Username : 
-                <input id='username' name='username' type="text" placeholder='username' />
-            </label>
-            <label htmlFor="password"> Password :  
-                <input id='password' name='password' type="password" placeholder='password' />
-            </label>
-            <button type='submit' onSubmit={handleSubmit}>Login</button>
+          <div>
+            <label htmlFor="username"> Username : </label>
+            <input id='username' name='username' type="text" placeholder='username' />
+          </div>
+          <div>
+            <label htmlFor="password"> Password : </label>
+            <input id='password' name='password' type="password" placeholder='password' />
+          </div>
+            <button type='submit' onClick={handleSubmit}>Login</button>
         </form>
     </>
   )
