43c81cf8bca7ae64171a875d647f172651295521
sarava338 <sarava338@gmail.com>
2023-03-18 11:54:55 +0530

    Modify: Modified styles in svg tags

--- src/assets/svg/RemoveFromPlayList.jsx
+++ src/assets/svg/RemoveFromPlayList.jsx
@@ -10,7 +10,7 @@ const RemoveFromPlayList = () => {
       height="24"
       viewBox="0,0,256,256"
       // eslint-disable-next-line react/style-prop-object
-      style="fill:#000000;"
+      style={{fill: '#000'}}
     >
       <defs>
         <clipPath id="clip-1_ADJNqbZj2z0G_gr1">
@@ -23,43 +23,43 @@ const RemoveFromPlayList = () => {
         </clipPath>
       </defs>
       <g
-        clip-path="url(#clip-1)"
+        clipPath="url(#clip-1)"
         fill="none"
-        fill-rule="nonzero"
+        fillRule="nonzero"
         stroke="none"
-        stroke-width="1"
-        stroke-linecap="butt"
-        stroke-linejoin="miter"
-        stroke-miterlimit="10"
-        stroke-dasharray=""
-        stroke-dashoffset="0"
-        font-family="none"
-        font-weight="none"
-        font-size="none"
-        text-anchor="none"
+        strokeWidth="1"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeMiterlimit="10"
+        strokeDasharray=""
+        strokeDashoffset="0"
+        fontFamily="none"
+        fontWeight="none"
+        fontSize="none"
+        textAnchor="none"
         // eslint-disable-next-line react/style-prop-object
-        style="mix-blend-mode: normal"
+        style={{mixBlendMode: 'normal'}}
       >
         <g transform="scale(10.66667,10.66667)" fill="#000000">
           <path d="M6,2c-1.15556,0 -2,0.96667 -2,2v18.03906l8,-2.97266l8,2.97266v-1.43945v-16.59961c0,-0.52222 -0.19133,-1.05461 -0.56836,-1.43164c-0.37703,-0.37703 -0.90942,-0.56836 -1.43164,-0.56836zM6,4h12v15.16211l-6,-2.22852l-6,2.22852z"></path>
         </g>
       </g>
       <g
-        fill="#000000"
-        fill-rule="nonzero"
+        fill="none"
+        fillRule="nonzero"
         stroke="none"
-        stroke-width="1"
-        stroke-linecap="butt"
-        stroke-linejoin="miter"
-        stroke-miterlimit="10"
-        stroke-dasharray=""
-        stroke-dashoffset="0"
-        font-family="none"
-        font-weight="none"
-        font-size="none"
-        text-anchor="none"
+        strokeWidth="1"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeMiterlimit="10"
+        strokeDasharray=""
+        strokeDashoffset="0"
+        fontFamily="none"
+        fontWeight="none"
+        fontSize="none"
+        textAnchor="none"
         // eslint-disable-next-line react/style-prop-object
-        style="mix-blend-mode: normal"
+        style={{mixBlendMode: 'normal'}}
       >
         <g transform="translate(160,176) scale(6.4,6.4)" id="overlay">
           <path d="M5,0c-2.761,0 -5,2.239 -5,5c0,2.761 2.239,5 5,5c2.761,0 5,-2.239 5,-5c0,-2.761 -2.239,-5 -5,-5zM4.5,8l-2.95,-2.95l1.4,-1.4l1.55,1.55l2.55,-2.55l1.4,1.4z"></path>
