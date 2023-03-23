93c83978055f2ce747b37772ec152bd070474cf4
sarava338 <sarava338@gmail.com>
2023-03-23 18:43:35 +0530

    Remove : Reoved the unwanted links in navbar

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -20,23 +20,13 @@ const Nav = () => {
       >
         <p>Sarava338-netflix-clone</p>
 
-        <nav className="page-links">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://github.com/sarava338/"
-            >
-              @sarava338
-            </a>
-          </ul>
-        </nav>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/sarava338/"
+        >
+          @sarava338
+        </a>
       </nav>
     </>
   );
