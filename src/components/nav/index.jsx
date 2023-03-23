022551fe4f172c4819951e6eedfb31e2e726ed06
sarava338 <sarava338@gmail.com>
2023-03-23 18:31:30 +0530

    Update: Added Nav backgroundColor transparent while scrolling

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,20 +1,32 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./nav.css";
 
 const Nav = () => {
+  const [bgColor, setBgColor] = useState("black");
+
+  window.addEventListener("scroll", () => {
+    window.scrollY >= 500 ? setBgColor("black") : setBgColor("transparent");
+  });
+
   return (
     <>
-      <nav className="navbar">
+      <nav
+        className="navbar"
+        style={{
+          backgroundColor: bgColor,
+        }}
+      >
         <p>Sarava338-netflix-clone</p>
 
         <nav className="page-links">
           <ul>
             <li>
-              <Link to='/'>Home</Link>
+              <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to='/login' >Login</Link>
+              <Link to="/login">Login</Link>
             </li>
             <a
               target="_blank"
